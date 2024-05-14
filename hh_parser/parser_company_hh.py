6a431281ee1debc1ce55e06599a6151839550865
Maxim Kapustyansky <max160996@mail.ru>
2024-05-14 16:03:10 +0300

    update company parser
    - Добавлена асинхронность работы кода, для ускорения работы
    - Добавлено отслеживание log процессов
    - Добавлен Semaphore (Рекомендуемое количество от 2 до 5)
    - Улучшена постановка ссылок для парсера (теперь он не идет по пустым ссылкам при смене page)
    - Изменены логи, они стали более информативными
    - Типизирован код для лучшего понимания со стороны
    - Изменено region -> regions

--- hh_parser/parser_company_hh.py
+++ hh_parser/parser_company_hh.py
@@ -1,122 +1,140 @@
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
-import requests
-from database import HHCompanyList
 from time import time
+import logging
 
+from database.database import HHCompanyList
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s %(asctime)s %(message)s")
 HEADERS = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-                            AppleWebKit/537.36 (KHTML, like Gecko)\
-                             Chrome/58.0.3029.110 Safari/537.3'
-        }
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                    AppleWebKit/537.36 (KHTML, like Gecko)\
+                     Chrome/58.0.3029.110 Safari/537.3'
+}
+
+with open('regions.txt', 'r') as file:                  # номера регионов
+    REGIONS = [line.strip() for line in file]
 
 
-def link_parser(url):
+async def parser(url: str) -> list or bool:
+    """
+    Парсинг страницы с компаниями, функция вытаскивает названия компаний
+
+    Args: url (str)
+
+    Returns: list[name] or bool(False)
+    :param url:
+    :return list[name]
+    :exception: логирует ошибки, возвращает False
+    """
     try:
-        response = requests.get(url, headers=HEADERS)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=HEADERS) as response:
+                html = await response.text()
+                soup = BeautifulSoup(html, 'html.parser')
 
-        companies_table = soup.select_one(
-            'div.bloko-column.bloko-column_container.bloko-column_xs-4.bloko-column_m-8.bloko-column_l-11 table tbody'
-        )
-        companies = companies_table.find_all('div')
-        print(f'[INFO] {url}')
-        result = []
-        for company in companies:
-            name_element = company.select_one('div.bloko-link')
-            name = name_element.text.strip() if name_element else company.select_one('a').text.strip()
-            result.append(name)
-        return result
+                companies_table = soup.select_one(              # необходимый блок с названиями компаний
+                    'div.bloko-column.bloko-column_container.bloko-column_xs-4.bloko-column_m-8.bloko-column_l-11 \
+                    table tbody'
+                )
+                companies = companies_table.find_all('div')
+                logging.info(f'статус: {response.status} | {url}')
+                result = []
+                for company in companies:
+                    name_element = company.select_one('div.bloko-link')
+                    name = name_element.text.strip() if name_element else company.select_one('a').text.strip()
+                    result.append(name)
+                return result
     except Exception as e:
-        print(f'[ERROR] {e}')
+        logging.error(f'ОШИБКА ЧТЕНИЯ СТРАНИЦЫ | {e}')
+        logging.error(f'{url}')
         return False
 
 
-"""
-разбор ссылки:
+async def process_page(semaphore, region, letter, page, base_url) -> None or bool:
+    """
+    Функция вызывает parser(), вытаскивает названия компаний и записывает их в БД
 
-https://hh.ru/                                                          домен
-employers_list?                                                         категория компании
-query=&hhtmFrom=employers_list                                          запрос из категории кампании
-&hhtmFromLabel=employer_search_line                                     из линии
-&areaId=2                                                               Город/Регион (0-все)
-&letter=Г                                                               Фильтр по алфавиту (А-Я)(A-Z)(%23)
-&page=2                                                                 Страницы (0-49)
-"""
+    Args: semaphore, region (str), letter (str), page (int), base_url (str)
+
+    :param semaphore:
+    :param region:
+    :param letter:
+    :param page:
+    :param base_url:
+    :return: None or bool(False)
+    """
+    async with semaphore:
+        start_time = time()
+        result_url = base_url + f"&areaId={region}&letter={letter}&page={page}"
+        _temp = await parser(result_url)
+        end_time = time()
+        if _temp:
+            # HHCompanyList.create(name=_temp)
+            for name in _temp:
+                with open('file.txt', 'a', encoding='utf-8') as f:
+                    f.write(f'{name}\n')
+            logging.info(f'{region=} {letter=} {page=} | {round(end_time - start_time, 3)} sec')
+        else:
+            return False
+
+
+async def paginator(base_url, session, letters, semaphore) -> None:
+    """
+    Функция вызывает process_page(), создает ссылки и формирует задачи для parser
+
+    Args: base_url (str), session (aiohttp.ClientSession), letters (list), semaphore (asyncio.Semaphore)
+
+    Returns: None
+
+    :param base_url:
+    :param session:
+    :param letters:
+    :param semaphore:
+    :return:
+    """
+    for region in REGIONS:
+        async with session.get(base_url + f"&areaId={region}", headers=HEADERS) as response:
+            html = await response.text()
+            soup = BeautifulSoup(html, 'html.parser')
+            count_class = soup.find('div', class_='totals--rE1moq2jhLukW5QVcI6L')
+            count_span = count_class.find('span', class_='bloko-text_strong')
+            count = int(count_span.text.strip())
+            logging.info(f'{count} компаний в регионе')
+
+        if count > 0:
+            if count > 5000:
+                for letter in letters:
+                    tasks = [process_page(semaphore, region, letter, page, base_url)
+                             for page in range(int(count/100)+1)]
+                    await asyncio.gather(*tasks)
+            else:
+                tasks = [process_page(semaphore, region, "", page, base_url)
+                         for page in range(int(count/100)+1)]
+                await asyncio.gather(*tasks)
 
 
 def get_alphabet() -> list:
     """
-    Получение списка ссылок на страницы алфавитного заголовка
-    Returns: &letter=A-Z-А-Я-%23
+    Функция формирует часть запроса для перехода по буквам алфавита и ссылке на числа %23
+
+    Args: None
+
+    Returns: list[str]
     """
-    # Генерация списка из всех заглавных букв английского алфавита
     english_uppercase = [chr(i) for i in range(65, 91)]
-    # Генерация списка из всех заглавных букв русского алфавита
     russian_uppercase = [chr(i) for i in range(1040, 1072)]
-    # Объединение списков
     all_uppercase = english_uppercase + russian_uppercase + ['%23']
     return ['&letter=' + i for i in all_uppercase]
 
 
-def generate_url():
-    """
-    Перебор ссылок и передача их в парсер
-    Returns:
-    """
+async def main():
     base_url = "https://hh.ru/employers_list?query=&hhtmFrom=employers_list&hhtmFromLabel=employer_search_line"
-    k = 0
-    letters = get_alphabet()
-
-    with open("region.txt", "r") as file:
-        # Читаем все строки из файла и удаляем символы новой строки (\n)
-        lines = [line.strip() for line in file]
-
-    for city in lines:
-        _url = base_url + "&areaId=" + str(city)
-        response = requests.get(_url, headers=HEADERS)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        count_class = soup.find('div', class_='totals--rE1moq2jhLukW5QVcI6L')
-        count_span = count_class.find('span', class_='bloko-text_strong')
-        _count = count_span.text.strip()
-        print(f'[INFO] {_count} компаний в городе')
-        if int(_count) > 0:
-            if int(_count) > 5000:
-                for letter in letters:
-                    for page in range(0, 50):
-                        start_time = time()
-                        result_url = base_url + "&areaId=" + str(city) + letter + "&page=" + str(page)
-                        _temp = link_parser(result_url)
-                        if _temp:
-                            for name in _temp:
-                                HHCompanyList.create(name=name)
-                                # print(name)
-                                pass
-                            k += 1
-                            print(f'[INFO] ~{k*100} строк')
-                        else:
-                            break
-                        end_time = time()
-                        print(f'[INFO] {round(end_time - start_time, 3)} sec')
-
-            else:
-                for page in range(0, 50):
-                    start_time = time()
-                    result_url = base_url + "&areaId=" + str(city) + "&page=" + str(page)
-                    _temp = link_parser(result_url)
-                    if _temp:
-                        for name in _temp:
-                            HHCompanyList.create(name=name)
-                            # print(name)
-                            pass
-                        k += 1
-                        print(f'[INFO] ~{k*100} строк')
-                    else:
-                        break
-                    end_time = time()
-                    print(f'[INFO] {round(end_time - start_time, 3)} sec')
-
+    letters = get_alphabet()          # Формируем список букв алфавита и ссылки на числа
+    semaphore = asyncio.Semaphore(2)  # Устанавливаем максимальное количество одновременных запросов
+    async with aiohttp.ClientSession() as session:
+        await paginator(base_url, session, letters, semaphore)
 
 if __name__ == '__main__':
-
-    generate_url()
-
+    asyncio.run(main())
