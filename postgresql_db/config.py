09bcd80a2bc3adad96831101fa4325295b9576b9
MaskedMir <135768927+MaskedMir@users.noreply.github.com>
2024-05-17 15:28:18 +0300

    config

--- postgresql_db/config.py
+++ postgresql_db/config.py
@@ -1,30 +1,9 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+# config.py
 
-
-class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
-
-    @property
-    def DATABASE_URL_psycopg2(self) -> object:
-        # postgres+psycopg2://postgres:postgres@localhost:5432/name
-        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    @property
-    def DATABASE_URL_asyncpg(self) -> object:
-        # postgres+psycopg2://postgres:postgres@localhost:5432/name
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    model_config = SettingsConfigDict(env_file="postgresql.env")
-
-
-settings = Settings(
-    DB_HOST="localhost",
-    DB_PORT=5432,
-    DB_USER="postgres",
-    DB_PASS="1234",
-    DB_NAME="localparser"
-)
+DB_USER = "user1"
+DB_PASS = "testpass"
+DB_HOST = "postgresql_host"
+DB_PORT = "6432"
+DB_NAME = "hr_parser_db"
+DB_SSLMODE = "require"
+SSLROOTCERT = "PG_GA_2.pem"
