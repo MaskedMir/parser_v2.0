773c5e649fe6783b2473859617f858a55f66115b
Maxim Kapustyansky <max160996@mail.ru>
2024-05-24 13:26:04 +0300

    json-vacancy

--- main.py
+++ main.py
@@ -464,8 +464,8 @@ async def run_parsers():
 
 
 def start_server():
-    # uvicorn.run(app, host="0.0.0.0", port=5000)
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
+    # uvicorn.run(app, host="127.0.0.1", port=5000)
 
 
 app.include_router(router)
