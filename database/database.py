4ea6c04feeb3bd22175c245ba25f4d36a693359d
Denis Nasonov <brain0991@gmail.com>
2024-05-27 12:43:41 +0300

    new branch

--- database/database.py
+++ database/database.py
@@ -18,7 +18,7 @@ db = PooledMySQLDatabase(
     host=DB_HOST,
     max_connections=10,  # максимальное количество соединений в пуле
     stale_timeout=300,  # время в секундах, через которое неиспользуемое соединение будет закрыто
-    ssl={'ca': '/database/MySQL.pem'}
+    ssl={'ca': r'C:\Users\Denis\PycharmProjects\pythonProject\dig-search-develop_2\database\MySQL.pem'}
     # ssl={'ca': r'C:\Users\Masked\PycharmProjects\dig-search-develop\database\MySQL.pem'}
     # ssl={'ca': r'C:\Users\max16\PycharmProjects\dig-search-develop_2\database\MySQL.pem'}
 )
